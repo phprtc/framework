054814ae8b899207d3beff386ef0807b186df891
ahmard <ahmard.mu@gmail.com>
2022-05-13 18:08:24 +0100

    Added decorative error output

--- server.php
+++ server.php
@@ -10,21 +10,25 @@ require __DIR__ . '/vendor/autoload.php';
 
 $env = Dotenv::createImmutable(__DIR__)->load();
 
-Server::create($env['SERVER_HOST'], $env['SERVER_PORT'])
-    ->setDocumentRoot(__DIR__ . '/public')
-    ->setHttpKernel(HttpKernel::class)
-    ->setWebsocketKernel(WSKernel::class)
-    ->onStart(function (\Swoole\Http\Server $server) use ($env) {
+try {
+    Server::create($env['SERVER_HOST'], $env['SERVER_PORT'])
+        ->setDocumentRoot(__DIR__ . '/public')
+        ->setHttpKernel(HttpKernel::class)
+        ->setWebsocketKernel(WSKernel::class)
+        ->onStart(function (\Swoole\Http\Server $server) use ($env) {
 
-        if ('development' == strtolower($env['APP_ENV'])) {
-            Watcher::create()
-                ->addPath(__DIR__ . '/app')
-                ->addPath(__DIR__ . '/routes')
-                ->addPath(__DIR__ . '/vendor')
-                ->onChange(fn() => $server->reload())
-                ->start();
-        }
+            if ('development' == strtolower($env['APP_ENV'])) {
+                Watcher::create()
+                    ->addPath(__DIR__ . '/app')
+                    ->addPath(__DIR__ . '/routes')
+                    ->addPath(__DIR__ . '/vendor')
+                    ->onChange(fn() => $server->reload())
+                    ->start();
+            }
 
-        echo "Server started at http://$server->host:$server->port\n";
-    })
-    ->run();
+            echo "Server started at http://$server->host:$server->port\n";
+        })
+        ->run();
+} catch (Throwable $e) {
+    console()->error($e);
+}
