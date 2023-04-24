1eed6aeb9ce70ae9765f45f4b70b5cf79a35f830
ahmard <ahmard.mu@gmail.com>
2023-04-24 20:28:56 +0100

    fix: example improvement

--- app/Websocket/Handlers/ChatWebsocketHandler.php
+++ app/Websocket/Handlers/ChatWebsocketHandler.php
@@ -7,6 +7,7 @@ use RTC\Contracts\Server\ServerInterface;
 use RTC\Contracts\Websocket\ConnectionInterface;
 use RTC\Contracts\Websocket\EventInterface;
 use RTC\Contracts\Websocket\FrameInterface;
+use RTC\Websocket\Exceptions\RoomOverflowException;
 use RTC\Websocket\Room;
 use RTC\Websocket\WebsocketHandler;
 use Throwable;
@@ -32,14 +33,18 @@ class ChatWebsocketHandler extends WebsocketHandler
     {
         $this->console->comment("Event: {$event->getEvent()} -> {$event->getFrame()->getRaw()}");
 
-        if ($event->eventIs('chat.room.join')) {
+        if ($event->eventIs('room.join')) {
             $this->console->writeln('Joined');
-            $connection->send('chat.forward', strtoupper($event->getMessage()));
+
         }
 
-        if ($event->eventIs('chat.room.message')) {
-            foreach ($this->getConnection())
-            $connection->send('chat.forward', strtoupper($event->getMessage()));
+        if ($event->eventIs('room.message')) {
+            $this->room->sendAsClient(
+                connection: $connection,
+                event: 'room.message',
+                message: $event->getMessage(),
+                meta: ['sender_name' => $connection->getIdentifier()]
+            );
         }
     }
 
@@ -47,12 +52,25 @@ class ChatWebsocketHandler extends WebsocketHandler
     {
     }
 
+    /**
+     * @param ConnectionInterface $connection
+     * @return void
+     * @throws RoomOverflowException
+     */
     public function onOpen(ConnectionInterface $connection): void
     {
         $this->addConnection($connection);
+
+        $this->room->add(
+            connection: $connection,
+            joinedMessage: sprintf('<b><i>%s</i></b> joined this chat', $connection->getIdentifier())
+        );
+
         $connection->send(
             event: 'welcome',
-            data: sprintf('Welcome<br/>This server has been running since %s.', $this->startDate)
+            data: [
+                'message' => sprintf('Welcome<br/>This server has been running since <b>%s</b>.', $this->startDate)
+            ]
         );
 
         $this->console->info("Connection opened: {$connection->getIdentifier()}");
